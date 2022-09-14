ec89ad9634bf9e6db3d658bcc9e3d76e801e5dcc
Furqan Abid <thisismefurqan@gmail.com>
2022-09-14 17:40:54 +0500

    Yeehaw

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,15 @@
+// 9/1/2022
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, sum;
+	cout << "Enter first number: \n";
+	cin >> a;
+	cout << "Enter second number: \n";
+	cin >> b;
+	sum = a + b;
+	cout << "Sum of two numbers is : "<<sum;
+	return 0;
+}
