6815f0bd6f94fb406ef7f4c77fef608c2cea4000
Furqan Abid <thisismefurqan@gmail.com>
2023-03-31 09:02:20 +0500

    Updated formula

--- SumOf1toN.cpp
+++ SumOf1toN.cpp
@@ -5,13 +5,9 @@ using namespace std;
 
 int main()
 {
-	int n, sum = 0;
+	int n, sum;
 	cout << "Enter number: " << endl;
 	cin >> n;
-	for (int i = 1;i <= n;)
-	{
-		sum += i;
-		i++;
-	}
+	sum = n * (n + 1) / 2;
 	cout << "Sum of 1 to " << n << " is: " << sum << endl;
 }
